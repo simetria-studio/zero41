7afb1843c0dbe71d00790dda694f5ba15d0d26d3
Felipe Cruz <felipephplow@gmail.com>
2022-04-06 11:01:52 -0300

    a

--- components/header.vue
+++ components/header.vue
@@ -48,12 +48,46 @@
             <a href="/"> <b-img src="~/assets/img/logo.png" alt=""></b-img></a>
           </div>
         </div>
-        <div class="btn-menu">
-          <button><i class="fas fa-bars"></i></button>
+        <div class="btn-menu" >
+          <button @click="toggleMenu"><i class="fas fa-bars"></i></button>
         </div>
       </div>
       <div>
-        <div class="d-flex mx-auto align-items-center justify-content-center pb-3">
+        <div
+          class="d-flex mx-auto align-items-center justify-content-center pb-3"
+        >
+          <div class="phone-top">
+            <h4>41 99999-9999</h4>
+          </div>
+          <div class="whatsapp-top">
+            <b-img
+              src="~/assets/img/whatsapp.svg"
+              alt=""
+              class="img-fluid"
+            ></b-img>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="menu" :class="{ active: isActive }">
+      <div class="menu-header text-right">
+        <button @click="toggleMenu" class="btn-close">
+          <i class="fas fa-window-close"></i>
+        </button>
+      </div>
+      <div class="menu-itens">
+        <b-navbar toggleable="lg" type="dark" variant="">
+          <b-navbar-nav>
+            <b-nav-item href="#">ESTOQUE</b-nav-item>
+            <b-nav-item href="#">VENDA SEU CARRO</b-nav-item>
+            <b-nav-item href="#">FINANCIE</b-nav-item>
+            <b-nav-item href="#">SOBRE</b-nav-item>
+            <b-nav-item href="#">CONTATO</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar>
+      </div>
+      <div>
+        <div class="d-flex mx-auto align-items-center justify-content-center">
           <div class="phone-top">
             <h4>41 99999-9999</h4>
           </div>
@@ -70,7 +104,19 @@
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+
+  methods: {
+    toggleMenu() {
+      this.isActive = !this.isActive;
+    },
+  },
+
+  mounted() {},
+};
 </script>
-<style lang="">
-</style>
